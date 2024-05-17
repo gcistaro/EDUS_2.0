2e805f82393b869f8b0fd65d6426bc2c302f47cb
gcistaro <giovannicistaro@gmail.com>
2024-05-17 14:26:43 +0200

    Fixing sign in source term

--- src/Simulation/Functional_SourceTerm.hpp
+++ src/Simulation/Functional_SourceTerm.hpp
@@ -7,34 +7,20 @@ std::function<void(BlockMatrix<std::complex<double>, R>&, double const&, BlockMa
     Calculate_TDHamiltonian(time);
     Output.fill(0.*im);
     commutator(Output, -im, H.get_Operator_R(), Input);
-    //convolution1(Output, -im, H.get_Operator_R(), Input);
-    //convolution1(Output, +im, Input, H.get_Operator_R() );
     
     //R operator 
+    #pragma omp parallel for schedule(dynamic)
     for(int iR=0; iR<Output.get_nblocks(); ++iR){
         auto& R = (*(Output.get_MeshGrid()))[iR].get("Cartesian");
         auto las = laser(time).get("Cartesian");
         auto DotProduct = las[0]*R[0] + las[1]*R[1] + las[2]*R[2];
-        //auto R = (*(Output.get_MeshGrid()))[iR].get("LatticeVectors");
-        //auto R0 = R[0]-20./2.;
-        //auto R1 = R[1]-20./2.;
-        //auto R_ = Coordinate<k>(R0,R1,0,"LatticeVectors");
-        //auto& RR = R_.get("Cartesian");
-        //auto DotProduct = las[0]*RR[0] + las[1]*RR[1] + las[2]*RR[2];
-
 
         for(int irow=0; irow<Output.get_nrows(); ++irow){
             for(int icol=0; icol<Output.get_ncols(); ++icol){
-                Output(iR,irow, icol) += -im*Input(iR,irow, icol)*DotProduct;
+                Output(iR,irow, icol) += im*Input(iR,irow, icol)*DotProduct;
             }
         }
     }
-
-    //std::cout << "Max of Input: " << *max(Input) << std::endl;
-    //std::cout << "H[0]: " << H.get_Operator_R()[0] << std::endl;
-    //std::cout << "H[1]: " << H.get_Operator_R()[1] << std::endl;
-    //std::cout << "Max of Output: " << *(max(Output))  << std::endl;
-
 };
 
 
