ab4beeac997977e5fc141d097cbbae806421004e
gcistaro <giovannicistaro@gmail.com>
2024-09-23 14:31:49 +0200

    Adding .gitignore

--- src/core/githash.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef GIT_HASH
-#define GIT_HASH 
-const char* const git_hash = "c77711b17963a4bb23ebe441f2dc710be986aeec";
-const char* const git_branchname = "remotes/origin/HEAD";
-const char* const build_date = "2024-04-11 09:27:57";
-#endif
-
-
