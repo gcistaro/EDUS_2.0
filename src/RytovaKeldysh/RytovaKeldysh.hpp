57a623b8e150133e5802ca7f141f90dde35800c6
gcistaro <giovannicistaro@gmail.com>
2024-09-23 14:32:59 +0200

    Updating RytovaKeldysh file

--- src/RytovaKeldysh/RytovaKeldysh.hpp
+++ src/RytovaKeldysh/RytovaKeldysh.hpp
@@ -20,9 +20,9 @@ double struve(const double& x, const double& v)
 }
 
 //computes Y_v(x)
+//this is contained in math.h
 
-
-class Coulomb
+class RytovaKeldysh
 {
     private:
         enum Dimensionality {twoD, threeD} dim;
@@ -30,17 +30,21 @@ class Coulomb
         double epsilon = 2.;
         double Area=1.;
 
-        Matrix<std::complex<double>> RytovaKeldish_TB;//it contains W[n]=W(rn)
+        mdarray<std::complex<double>,4> RytovaKeldysh_TB;
+        std::shared_ptr<MeshGrid> Rgrid;
     public:
-        Coulomb(const std::array<Operator<std::complex<double>>,3>& r, const int& dim_);
-        std::complex<double> W(const Coordinate<k>& q);
-        std::complex<double> W(const Coordinate<R>& r);
+        RytovaKeldysh(){};
+        RytovaKeldysh(const std::array<Operator<std::complex<double>>,3>& r, const int& dim_,
+                     const std::shared_ptr<MeshGrid>& MasterRGrid);
+        //std::complex<double> W(const Coordinate& q);
+        std::complex<double> Potential(const Coordinate& r);
 
 
 };
 
 
-Coulomb::Coulomb(const std::array<Operator<std::complex<double>>,3>& r, const int& dim_)//Pass rR[R=0]
+RytovaKeldysh::RytovaKeldysh(const std::array<Operator<std::complex<double>>,3>& r, const int& dim_,            
+                             const std::shared_ptr<MeshGrid>& MasterRGrid)
 {
     if(dim_ != 2){
         std::cout << "Warning ! Only 2d coulomb is implemented!\n";
@@ -50,28 +54,50 @@ Coulomb::Coulomb(const std::array<Operator<std::complex<double>>,3>& r, const in
     if(dim_==2) dim=twoD;
     if(dim_==3) dim=threeD;
 
-    auto& x = (r[0].get_Operator_R())[Coordinate<R>(0,0,0)];
-    auto& y = (r[1].get_Operator_R())[Coordinate<R>(0,0,0)];
-    auto& z = (r[2].get_Operator_R())[Coordinate<R>(0,0,0)];
+    auto& x0 = (r[0].get_Operator_R())[0];
+    auto& y0 = (r[1].get_Operator_R())[0];
+    auto& z0 = (r[2].get_Operator_R())[0];
     
     //I use W(n'R1', m'R2', nR1, mR2) = 1/Omega^2*delta(nn')delta(R1R1')delta(mm')delta(R2R2')W(rn+R1-rm-R2)
-    RytovaKeldish_TB.initialize(x.get_nrows(), x.get_ncols());
-    for(int in=0; in<RytovaKeldish_TB.get_nrows(); in++){
-        for(int im=0; im<RytovaKeldish_TB.get_ncols(); im++){
-            auto n_atom = Coordinate<R>((r[0].get_Operator_R())[Coordinate<R>(0,0,0)](in,in).real(),
-                                               (r[1].get_Operator_R())[Coordinate<R>(0,0,0)](in,in).real(),
-                                               (r[2].get_Operator_R())[Coordinate<R>(0,0,0)](in,in).real());
-            auto m_atom = Coordinate<R>((r[0].get_Operator_R())[Coordinate<R>(0,0,0)](in,in).real(),
-                                               (r[1].get_Operator_R())[Coordinate<R>(0,0,0)](in,in).real(),
-                                               (r[2].get_Operator_R())[Coordinate<R>(0,0,0)](in,in).real());
+    RytovaKeldysh_TB.initialize({MasterRGrid->get_TotalSize(),
+                                 MasterRGrid->get_TotalSize(),
+                                 r[0].get_Operator_R().get_nrows(), 
+                                 r[0].get_Operator_R().get_ncols()});
+    Rgrid = MasterRGrid;
 
-            RytovaKeldish_TB(in, im) = W(n_atom - m_atom);
+    //Calculate values of the potential
+    std::cout << "RytovaKeldysh_TB.get_Size(0): " << RytovaKeldysh_TB.get_Size(0) << std::endl;
+    std::cout << "RytovaKeldysh_TB.get_Size(1): " << RytovaKeldysh_TB.get_Size(1) << std::endl;
+
+    #pragma omp parallel for schedule(static)
+    for(int iR=0; iR<RytovaKeldysh_TB.get_Size(0); ++iR) {
+        auto& Rcart = (*Rgrid)[iR].get("Cartesian");
+        for(int iS=0; iS<RytovaKeldysh_TB.get_Size(1); ++iS) {
+            auto& Scart = (*Rgrid)[iS].get("Cartesian");
+            for(int in=0; in<RytovaKeldysh_TB.get_Size(2); in++){
+                //ratom_n = rn + R
+                auto ratom_n = Coordinate(x0(in,in).real() + Rcart[0],
+                                          y0(in,in).real() + Rcart[1],
+                                          z0(in,in).real() + Rcart[2]);
+
+                for(int im=0; im<RytovaKeldysh_TB.get_Size(3); im++){
+                    //ratom_m = rm + S
+                    auto ratom_m = Coordinate(x0(im,im).real()+Scart[0],
+                                              y0(im,im).real()+Scart[1],
+                                              z0(im,im).real()+Scart[2]);
+
+                    RytovaKeldysh_TB(iR, iS, in, im) = Potential(ratom_n - ratom_m);
+                }
+            }
         }
     }
+    std::cout << "out of loop!\n";
 }
 
-std::complex<double> Coulomb::W(const Coordinate<k>& q)
+/*
+std::complex<double> Coulomb::W(const Coordinate& q)
 {
+
     double qnorm = q.norm();
     std::complex<double> Wq;
     switch(dim)
@@ -90,8 +116,8 @@ std::complex<double> Coulomb::W(const Coordinate<k>& q)
     }
     return Wq;
 }
-
-std::complex<double> Coulomb::W(const Coordinate<R>& r)
+*/
+std::complex<double> RytovaKeldysh::Potential(const Coordinate& r)
 {
     std::complex<double> Wr;
     auto r_norm = r.norm();
@@ -144,13 +170,9 @@ std::complex<double> Coulomb::W(const Coordinate<R>& r)
             std::cout << "Not implemented!\n";
             exit(1);
         }
+        default:
+            break;
     }
     return Wr;
 }
-
-
-
-//Hartree: H_{nn'}(R1) = \sum_m \delta{nn'} W(rn-rm+R1) \rho_{mm}(0)
-
-//Fock: H_{nn'}(R1) = 
 
