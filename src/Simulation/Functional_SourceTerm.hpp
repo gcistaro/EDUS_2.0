087a54766fb0172dea78fb6d08aa73f482018710
gcistaro <giovannicistaro@gmail.com>
2024-09-25 16:16:36 +0200

    Fixing the source term calculation

--- src/Simulation/Functional_SourceTerm.hpp
+++ src/Simulation/Functional_SourceTerm.hpp
@@ -7,35 +7,39 @@ SourceTerm =
     // P <- Input
     PROFILE("RK::SourceTerm");
 
+    //go to R to calculate Coulomb and R term
     H.go_to_R(false);
-    Output.go_to_R();
+    Output.go_to_R(false);
     const_cast<Operator<std::complex<double>>&>(Input).go_to_R();
+    Output.get_Operator(R).fill(0.);
 
-    // Output +=   (E.Nabla) * Input
+    //Output +=   (E.Nabla) * Input
     PROFILE_START("i*(E.R)*Input");
     kgradient.Calculate(Output.get_Operator(Space::R), 
                         Input.get_Operator(Space::R), 
-                        laser(time), false);
+                        laser(time), true);
     PROFILE_STOP("i*(E.R)*Input");
 
     PROFILE_START("Coulomb");
-    coulomb.EffectiveHamiltonian(H, Input, true);
+    coulomb.EffectiveHamiltonian(H, Input, false);
     PROFILE_STOP("Coulomb");
 
+    //go to k to calculate commutator term
     Output.go_to_k();
     const_cast<Operator<std::complex<double>>&>(Input).go_to_k(false);
     H.go_to_k();
 
     // H = H0 + E.r
-    Calculate_TDHamiltonian(time, !coulomb.get_DoCoulomb() );
+    Calculate_TDHamiltonian(time, false );
     
     // Output = -i * [ H, Input ]
     auto& Output_ = Output.get_Operator(Space::k);
     auto& Input_ = Input.get_Operator(Space::k);
     auto& H_ = H.get_Operator(Space::k);
     
-    Output_.fill(0.*im);
-    commutator(Output_, -im, H_, Input_);
+    //Output_.fill(0.*im);
+    commutator(Output_, -im, H_, Input_, false);
+
     //assert(Output_.is_hermitian());
     //std::cout << *max(Output_) << std::endl;
 };
