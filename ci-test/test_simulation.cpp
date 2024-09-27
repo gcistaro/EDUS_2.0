8c9f325fc906e74537ab811ce19c29d6a4fc21d7
gcistaro <giovannicistaro@gmail.com>
2024-09-27 12:45:07 +0200

    updating tesst_simulation with adaptive final time

--- ci-test/test_simulation.cpp
+++ ci-test/test_simulation.cpp
@@ -6,15 +6,16 @@ int main()
 {   
     initialize();
     PROFILE_START("test_simulation");
-    auto Rgrid = std::array<int,3>({20,20,1});
+    auto Rgrid = std::array<int,3>({100,100,1});
     Simulation simulation("/home/gcistaro/NEGF/tb_models/hBN_gap7.25eV_a2.5A", Rgrid);
 
     simulation.laser.set_Intensity(1.e+05, Wcm2);
-    //simulation.laser.set_Lambda(800, NanoMeters);
-    //simulation.laser.set_NumberOfCycles(10);
-    simulation.RK_object.set_ResolutionTime(0.1);//0.0003);
+    simulation.laser.set_Lambda(3000, NanoMeters);
+    simulation.laser.set_NumberOfCycles(3);
+    simulation.RK_object.set_ResolutionTime(simulation.laser.get_Duration()/simulation.laser.get_NumberOfCycles()/1000);
 
-   for(int it=0; it<1.e+04; ++it) {//simulation.RK_object.get_CurrentTime() < 100) {
+    auto index_FinalTime = simulation.laser.get_Duration()/simulation.RK_object.get_ResolutionTime() +1;
+    for(int it=0; it<index_FinalTime; ++it) {//simulation.RK_object.get_CurrentTime() < 100) {
         simulation.Propagate();
     }
     PROFILE_STOP("test_simulation");
