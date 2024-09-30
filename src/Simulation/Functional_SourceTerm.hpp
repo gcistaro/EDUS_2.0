dc141aac30fb9884b344ef0306899c5ddcc4f23e
gcistaro <giovannicistaro@gmail.com>
2024-09-30 12:13:32 +0200

    Update source term so that it works with scalar in gradient

--- src/Simulation/Functional_SourceTerm.hpp
+++ src/Simulation/Functional_SourceTerm.hpp
@@ -3,21 +3,26 @@ SourceTerm =
 [&](Operator<std::complex<double>>& Output, const double& time, const Operator<std::complex<double>>& Input)
 {
     // Output = -i * [ H, Input ]
+    const_cast<Operator<std::complex<double>>&>(Input).get_Operator_k().make_hermitian();
+    Calculate_TDHamiltonian(time, true);
+    Output.go_to_k(false);
+    const_cast<Operator<std::complex<double>>&>(Input).go_to_k(false);
+    H.go_to_k(false);
     auto& Output_ = Output.get_Operator(Space::k);
     auto& Input_ = Input.get_Operator(Space::k);
     auto& H_ = H.get_Operator(Space::k);
     
     commutator(Output_, -im, H_, Input_, true);
-
+    assert(Output_.is_hermitian());
     Output.go_to_R();
     const_cast<Operator<std::complex<double>>&>(Input).go_to_R();
     
     //Output +=   (E.Nabla) * Input
-    PROFILE_START("i*(E.R)*Input");
-    kgradient.Calculate(Output.get_Operator(Space::R), 
+    PROFILE_START("i(E.R)*Input");
+    kgradient.Calculate(1.+0.*im,Output.get_Operator(Space::R), 
                         Input.get_Operator(Space::R), 
                         laser(time), false);
-    PROFILE_STOP("i*(E.R)*Input");
+    PROFILE_STOP("i(E.R)*Input");
     const_cast<Operator<std::complex<double>>&>(Input).go_to_k(false);
     Output.go_to_k();
 };
