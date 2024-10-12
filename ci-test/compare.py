3334b3d4878cdc052877deb6670ec082a29ed04a
gcistaro <giovannicistaro@gmail.com>
2024-10-12 11:07:54 +0200

    Fixing hbn coulomb test

--- ci-test/compare.py
+++ ci-test/compare.py
@@ -10,7 +10,15 @@ arg1 = arg1[1:].flatten()
 arg2 = np.loadtxt(file2, unpack=True)
 arg2 = arg2[1:].flatten()
 
+print("comparing ", file1, " with ", file2)
+passed = True
 for i in range(arg1.shape[0]):
     if ( np.logical_and( np.abs(arg1[i]) > 1.e-09 , np.abs( arg1[i]-arg2[i] ) > 1.e-09 ) ):
         print(arg1[i], arg2[i], np.abs(arg1[i]-arg2[i]))
         sys.exit(1)
+        passed = False
+
+if(passed):
+    print("... Passed!")
+else:
+    print("... Not Passed!")
