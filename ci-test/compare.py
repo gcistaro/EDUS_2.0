5276f6acc05d5202a62d351c14347134f6d307ef
Giovanni <giovannicistaro@gmail.com>
2024-10-30 12:52:35 +0100

    Adding additional condition to compare.py

--- ci-test/compare.py
+++ ci-test/compare.py
@@ -13,10 +13,11 @@ arg2 = arg2[1:].flatten()
 print("comparing ", file1, " with ", file2)
 passed = True
 for i in range(arg1.shape[0]):
-    if ( np.logical_and( np.abs(arg1[i]) > 1.e-09 , np.abs( arg1[i]-arg2[i] ) > 1.e-09 ) ):
-        print(arg1[i], arg2[i], np.abs(arg1[i]-arg2[i]))
-        sys.exit(1)
-        passed = False
+    if ( np.abs( arg1[i]-arg2[i] ) > 1.e-09  ):
+        if ( np.logical_and ( np.abs(arg1[i]) < 1.e-09 , np.abs(arg2[i]) < 1.e-09 ) ):
+            print(arg1[i], arg2[i], np.abs(arg1[i]-arg2[i]))
+            sys.exit(1)
+            passed = False
 
 if(passed):
     print("... Passed!")
