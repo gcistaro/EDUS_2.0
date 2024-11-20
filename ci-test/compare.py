aba35cc2e86e44679a843dcc1f4d79d161c43d7d
gcistaro <giovannicistaro@gmail.com>
2024-11-20 16:22:26 +0100

    Wcm2 redefined

--- ci-test/compare.py
+++ ci-test/compare.py
@@ -6,15 +6,13 @@ file2 = sys.argv[2]
 
 arg1 = np.loadtxt(file1, unpack=True)
 arg1 = arg1[1:].flatten()
-
 arg2 = np.loadtxt(file2, unpack=True)
 arg2 = arg2[1:].flatten()
-
 print("comparing ", file1, " with ", file2)
 passed = True
 for i in range(arg1.shape[0]):
-    if ( np.abs( arg1[i]-arg2[i] ) > 1.e-09  ):
-        if ( np.logical_and ( np.abs(arg1[i]) < 1.e-09 , np.abs(arg2[i]) < 1.e-09 ) ):
+    if ( np.abs( arg1[i]-arg2[i] ) > 1.e-10  ):
+        if ( np.logical_or ( np.abs(arg1[i]) > 1.e-10 , np.abs(arg2[i]) > 1.e-10 ) ):
             print(arg1[i], arg2[i], np.abs(arg1[i]-arg2[i]))
             sys.exit(1)
             passed = False
--- src/ConvertUnits.cpp
+++ src/ConvertUnits.cpp
@@ -10,7 +10,7 @@ Unit Angstrom(LENGTH,1.e-10);
 Unit NanoMeters(LENGTH, 1.e-09);
 Unit ElectronVolt(ENERGY,ElectronVolt_value);
 Unit Joule(ENERGY,1.);
-Unit Wcm2(INTENSITY, 1.);
+Unit Wcm2(INTENSITY, 1.e+04);
 Unit FemtoSeconds(TIME, 1.e-15);
 
 Unit AuIntensity(INTENSITY, AtomicUnitOfIntensity);
