a05f60e04eb84d02a09d57cfc84977255fe80e92
Miguel Sá <1996.sa.miguel@gmail.com>
2025-03-10 18:27:16 +0100

    Absorbance automatic indexes added in "Postproces/Current.py" and "Postproces/custom_functions/absorbance.py". Silicon json and QE TB file added.

--- Postproces/custom_functions/absorbance.py
+++ Postproces/custom_functions/absorbance.py
@@ -3,8 +3,10 @@ import numpy as np
 from custom_functions.fouriertransform import FourierTransform
 from scipy import constants
 import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+import plotly.io as pio
 
-def absorbance(t_au, Vt_au, Et_au, limits):
+def absorbance(t_au, Vt_au, Et_au, limits=[]):
     print("Fourier transforming")
     freq_eV, Jw_au = FourierTransform(t_au, Vt_au, True)
     _, Ew_au       = FourierTransform(t_au, Et_au, False)
@@ -12,7 +14,29 @@ def absorbance(t_au, Vt_au, Et_au, limits):
 
 
     #cut a window
-    index = np.where(np.logical_and( freq_eV > limits[0], freq_eV < limits[1] ))[0]
+    if limits == []:
+        Ew_au_abs_max = np.max(np.abs(Ew_au))
+        Ew_au_condition = np.zeros(len(freq_eV))
+        for i in range(3):
+            Ew_au_condition += np.abs(np.abs(Ew_au[i,:]) >= 0.05*Ew_au_abs_max)
+        freq_eV_condition = freq_eV >= 0.0
+        meaningful_Ew_au_condition = freq_eV_condition*Ew_au_condition
+        index = np.where(meaningful_Ew_au_condition != 0)[0]
+    else:
+        index = np.where(np.logical_and( freq_eV > limits[0], freq_eV < limits[1] ))[0]
+
+    plt.plot(freq_eV, np.abs(Ew_au[0, :]))
+    plt.plot(freq_eV[index], np.abs(Ew_au[0, index]))
+    plt.xlim(left=0)
+    plt.show()
+    plt.close()
+
+    Ew_au_fig = go.Figure()
+    Ew_au_fig.add_trace(go.Scatter(x=freq_eV, y=np.abs(Ew_au[0, :]), mode='lines'))
+    Ew_au_fig.add_trace(go.Scatter(x=freq_eV[index], y=np.abs(Ew_au[0, index]), mode='lines'))
+    Ew_au_fig.update_layout(xaxis=dict(range=[0, 2*max(freq_eV[index])]))
+    pio.write_html(Ew_au_fig, 'Ew_au.html')
+
     Jw_au = Jw_au[:,index]
     Ew_au = Ew_au[:,index]
     freq_eV = freq_eV[index]
@@ -24,7 +48,9 @@ def absorbance(t_au, Vt_au, Et_au, limits):
     rw_au = 1j*freq_au*Jw_au
     Sw_au  = -(2*np.imag( np.sum( [ np.conj(Ew_au[ix])*rw_au[ix] for ix in [0,1,2] ], axis=0) ) )        #response function
     Iw_au = 2*np.sum( [np.abs(Ew_au[ix])*np.abs(Ew_au[ix]) for ix in [0,1,2] ], axis=0)/(4.*np.pi*alpha*freq_au)
-    plt.plot(freq_au, rw_au[0])
+    plt.plot(freq_eV, np.real(rw_au[0]), label = r'Real($\tilde{\left\langle \mathbf{r}\right\rangle }(\omega)$)')
+    plt.plot(freq_eV, np.imag(rw_au[1]), label = r'Imag($\tilde{\left\langle \mathbf{r}\right\rangle }(\omega)$)')
+    plt.legend()
     plt.show()
     
     print("Ew[1] min and max: ", np.min(np.abs(Ew_au[1])), np.max(np.abs(Ew_au[1])))
