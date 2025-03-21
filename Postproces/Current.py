64db0d831672ac0743e286256b6ce634707128f5
Miguel Sá <1996.sa.miguel@gmail.com>
2025-03-21 14:27:17 +0100

    1. Changed "EDUS_2.0/Postproces/custom_functions/absorbance.py" to: 	1.1. select meaningfull range of absorbance based on the absolute value of the electric field (this can be overridden by specifying the limits in the absorbance function as before) 	1.2. print in a .html file the absolute value of full electric field (right now only x-component) vs the one used for absorbance 	1.3. compute linear conductivity and plot it (should look very similar to absorbance up to a multiplicative factor) 	1.4. compute rw_au instead of dw_au and use this in the formula used for the absorbance
    2. Changed "EDUS_2.0/Postproces/Current.py" to:
    	2.1. integrate the changes listed above in "EDUS_2.0/Postproces/custom_functions/absorbance.py"
    	2.2. plot the absorbance in a .html file

--- Postproces/Current.py
+++ Postproces/Current.py
@@ -4,6 +4,8 @@ import numpy as np
 from numpy import fft
 import matplotlib.pyplot as plt
 import sys
+import plotly.graph_objects as go
+import plotly.io as pio
 
 from custom_functions.read import read_observables
 from custom_functions.absorbance import absorbance
@@ -22,7 +24,7 @@ t_au, _, Et_au, Vt_au = read_observables(folder, version)
 
 plt.plot(t_au, Vt_au[0])
 plt.show()
-freq_eV, Absorbance = absorbance(t_au, Vt_au, Et_au,[1,7])
+freq_eV, Absorbance = absorbance(t_au, Vt_au, Et_au)
 
 
 plt.plot(freq_eV, Absorbance, label="Abs(w)")
@@ -31,3 +33,9 @@ plt.legend()
 plt.show()
 np.savetxt("absorbance.txt", np.array(np.transpose([freq_eV, Absorbance])))
 plt.savefig("Absorbance.png", dpi=200)
+plt.close()
+
+absorbance_fig = go.Figure()
+absorbance_fig.add_trace(go.Scatter(x=freq_eV, y=Absorbance, mode='lines'))
+pio.write_html(absorbance_fig, 'absorbance.html')
+
