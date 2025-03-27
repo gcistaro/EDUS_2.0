8cc7d3dac4fd0f5536165142130f1b75174bd510
Mravran <mrawran@gmail.com>
2025-03-27 15:53:58 +0100

    Adress the division by 0 in Absorbance calculation

--- Postproces/custom_functions/absorbance.py
+++ Postproces/custom_functions/absorbance.py
@@ -27,6 +27,12 @@ def get_absorbance(t_au, Vt_au, Et_au, limits=[], smearing=0.):
     else:
         #take only values in a window of frequency that you can define in the input
         index = np.where(np.logical_and( freq_eV > limits[0], freq_eV < limits[1] ))[0]
+        ######## ONLY USE TO PREVENT DIVISION BY 0 ##########
+        for freq in range(len(Ew_au)):                    #
+                    if np.abs(Ew_au[freq]) < 1e-14:       #
+                        Ew_au[freq] = 0 # set to true 0   #
+        #####################################################
+
 
     #define everything only in the window
     Vw_au   = Vw_au[:,index]
@@ -44,6 +50,7 @@ def get_absorbance(t_au, Vt_au, Et_au, limits=[], smearing=0.):
     
     #spectral function of the electric field
     Iw_au = 2*np.sum( [np.abs(Ew_au[ix])*np.abs(Ew_au[ix]) for ix in [0,1,2] ], axis=0)/(4.*np.pi*alpha*freq_au) # not sure about these constants
-    Absorbance =Sw_au/Iw_au
+
+    Absorbance = np.divide(Sw_au,Iw_au, np.zeros_like(Sw_au, dtype = float), where=Iw_au!=0) # If divide by 0, Absorbance is set to 0.
 
     return freq_eV, Absorbance
