b6476882627c78ea7e393e285d26b629f23d5e75
Miguel Sá <1996.sa.miguel@gmail.com>
2025-03-27 10:27:41 +0100

    Included files from CoulombKCW to compute interactions based on screening

--- src/ConvertUnits.cpp
+++ src/ConvertUnits.cpp
@@ -9,6 +9,7 @@ Unit::Unit(const Type& type_, const double& value_) : type(type_), value(value_)
 Unit Angstrom(LENGTH,1.e-10);
 Unit NanoMeters(LENGTH, 1.e-09);
 Unit ElectronVolt(ENERGY,ElectronVolt_value);
+Unit Hartree(ENERGY,Hartree_value);
 Unit Joule(ENERGY,1.);
 Unit Wcm2(INTENSITY, 1.e+04);
 Unit FemtoSeconds(TIME, 1.e-15);
@@ -21,17 +22,18 @@ Unit NullUnit(NullType, 0.);
 
 Unit unit( const std::string& to_unit)
 {
-    if( to_unit == "angstrom"    )    {return Angstrom; }
+    if( to_unit == "angstrom"    )    {return Angstrom;    }
     if( to_unit == "nanometers"  )    {return NanoMeters;  }
     if( to_unit == "nm"          )    {return NanoMeters;  }
-    if( to_unit == "electronvolt")    {return ElectronVolt;  }
-    if( to_unit == "joule"       )    {return Joule;  }
-    if( to_unit == "wcm2"        )    {return Wcm2;  }
-    if( to_unit == "femtoseconds")    {return FemtoSeconds;  }
-    if( to_unit == "fs")              {return FemtoSeconds;  }
+    if( to_unit == "electronvolt")    {return ElectronVolt;}
+    if( to_unit == "joule"       )    {return Joule;       }
+    if( to_unit == "wcm2"        )    {return Wcm2;        }
+    if( to_unit == "femtoseconds")    {return FemtoSeconds;}
+    if( to_unit == "fs"          )    {return FemtoSeconds;}
     if( to_unit == "auintensity" )    {return AuIntensity; }
-    if( to_unit == "autime"      )    {return AuTime;  }
-    if( to_unit == "aulength"    )    {return AuLength;  }
-    if( to_unit == "auenergy"    )    {return AuEnergy;  }
+    if( to_unit == "autime"      )    {return AuTime;      }
+    if( to_unit == "aulength"    )    {return AuLength;    }
+    if( to_unit == "auenergy"    )    {return AuEnergy;    }
+    if( to_unit == "hartree"     )    {return Hartree;     }
     return NullUnit;
 }
