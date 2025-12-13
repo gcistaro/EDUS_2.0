d608b30df782b1c65ec150491b0f9f94ed788660
Giovanni <giovannicistaro@gmail.com>
2025-12-13 09:00:19 +0100

    Clean up and restructure the calculation of d\rho/dt in the time propagation (SourceTerm)

--- src/Simulation/Functional_SourceTerm.hpp
+++ src/Simulation/Functional_SourceTerm.hpp
@@ -11,14 +11,11 @@ std::function<void(Operator<std::complex<double>>&, double const&, Operator<std:
 SourceTerm = 
 [&](Operator<std::complex<double>>& Output__, const double& time__, const Operator<std::complex<double>>& Input__)
 {
+    /* next line aligns k and R components of input (DM_{i-1}) */
+    const_cast<Operator<std::complex<double>>&>(Input__).go_to_R(true);
     
-    if ( SpaceOfPropagation_Gradient_ == R ) {
-        Output__.go_to_R(false);
-        const_cast<Operator<std::complex<double>>&>(Input__).go_to_R(true);
-    }
-
-    /* Gradient term:    Output +=   (E.Nabla) * Input 
-    it acts only if we don't do Peierls transformation */   
+    /* Gradient term:    Output +=   (E.Nabla) * Input */ 
+    Output__.lock_space(SpaceOfPropagation_Gradient_);
     if (!ctx_->cfg().peierls()) {
         kgradient_.Calculate(1.+0.*im, Output__.get_Operator(SpaceOfPropagation_Gradient_), 
                         Input__.get_Operator(SpaceOfPropagation_Gradient_), 
@@ -26,25 +23,26 @@ SourceTerm =
 
     }
 
-    /* Coulomb interaction */
-    H_.go_to_R(false);
-    H_.get_Operator_R().fill(0);
-    coulomb_.EffectiveHamiltonian( H_, Input__, true); 
-
-    if ( SpaceOfPropagation_Gradient_ == R ) {
-        Output__.go_to_k(true);
-        const_cast<Operator<std::complex<double>>&>(Input__).go_to_k(false);
+    /* IPA Hamiltonian H_ = H0_ + E \cdot r*/
+    Calculate_TDHamiltonian(time__, true);
+    
+    /* Peierls transformation H_(R) = H_(R)*exp(+i*A(t) \cdot R) */
+    if(ctx_->cfg().peierls()) {
+        apply_peierls_phase(H_, time__, +1);
     }
 
+    /* Coulomb interaction H_ += \Sigma^H + \Sigma^{SEX} */
+    H_.go_to_R();
+    coulomb_.EffectiveHamiltonian( H_, Input__, false); 
 
-    // Output += -i * [ H, Input ]
-    if( SpaceOfCalculateTDHamiltonian_ == k ) H_.go_to_k(true);
-    Calculate_TDHamiltonian(time__, false);
-    if( SpaceOfCalculateTDHamiltonian_ == R ) H_.go_to_k(true);
+
+    // Output__ += -i * [ H_, Input__ ]
+    Output__.go_to_k();
+    H_.go_to_k();
+    const_cast<Operator<std::complex<double>>&>(Input__).lock_space(Space::k); //this is already updated, no need to ft
 
     auto& Output = Output__.get_Operator(SpaceOfPropagation_);
     auto& Input = Input__.get_Operator(SpaceOfPropagation_);
     auto& H = H_.get_Operator(SpaceOfPropagation_);
     commutator(Output, -im, H, Input, false);
-
 };
